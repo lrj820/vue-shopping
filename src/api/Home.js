1c2e707ceb056f17702557bb04419e9f83a7a61c
‘lrj820ʼ <19876321348@163.com>
2024-07-15 23:47:54 +0800

    新鲜好物的实现

--- src/api/Home.js
+++ src/api/Home.js
@@ -1,2 +1,3 @@
 import instance from '@/utils/request'
 export const BannerService = (distributionSite) => instance.get('/home/banner',distributionSite )
+export const NewService = (limit) => instance.get('/home/new',limit)
