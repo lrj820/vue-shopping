9610e8dd59c8377493a64a1b323ef002b16bd90c
‘lrj820ʼ <19876321348@163.com>
2024-07-16 13:44:09 +0800

    product列表渲染

--- src/api/Home.js
+++ src/api/Home.js
@@ -2,3 +2,4 @@ import instance from '@/utils/request'
 export const BannerService = (distributionSite) => instance.get('/home/banner',distributionSite )
 export const NewService = (limit) => instance.get('/home/new',limit)
 export const HotService = () =>instance.get('/home/hot')
+export const ProductService = () =>instance.get('/home/goods')
--- src/main.js
+++ src/main.js
@@ -5,26 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
+import {lazyPlugin} from '@/directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
 
-// 使 v-focus 在所有组件中都可用
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        console.log(el,binding.value)
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-            if(isIntersecting){
-                el.src = binding.value
-            }
-            },
-          )
-    },
-})
+
