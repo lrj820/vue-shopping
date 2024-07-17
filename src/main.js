fa5fb401f908ec1ea64da30a5223ef78d051b30c
‘lrj820ʼ <19876321348@163.com>
2024-07-18 00:20:10 +0800

    pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import {lazyPlugin} from '@/directives'
@@ -10,10 +10,11 @@ import {lazyPlugin} from '@/directives'
 // 引入全局组件插件
 import { componentPlugin } from '@/components'
 
-
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
 app.use(componentPlugin)
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.mount('#app')
