7744ce778c6bcef21b0ecf71023416e1630e24f1
‘lrj820ʼ <19876321348@163.com>
2024-07-17 13:48:34 +0800

    详情整体业务认识和路由配置

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -17,7 +17,7 @@ onMounted(() => {
   <HomePanel title="新鲜好物" subtitle="新鲜出炉,品质靠谱">
     <ul class="goods-list">
     <li v-for="item in NewList" :key="item.id">
-      <RouterLink to="/">
+      <RouterLink :to="`/detail/${item.id}`">
         <img v-img-lazy="item.picture" alt="" />
         <p class="name">{{ item.name }}</p>
         <p class="price">&yen;{{ item.price }}</p>
