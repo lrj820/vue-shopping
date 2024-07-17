50c07257b6dd302662021c318f882b3aadc5d07f
‘lrj820ʼ <19876321348@163.com>
2024-07-17 22:53:35 +0800

    登陆的表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -25,6 +25,14 @@ const rules = {
     }
   ]
 }
+const formRef = ref(null)
+const Dologin =() =>{
+  formRef.value.validate((vaild) => {
+    if(vaild){
+      //
+    }
+  })
+}
 </script>
 
 
@@ -49,7 +57,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account"  label="账户">
                 <el-input v-model="form.account"/>
@@ -62,7 +70,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="Dologin">点击登录</el-button>
             </el-form>
           </div>
         </div>
