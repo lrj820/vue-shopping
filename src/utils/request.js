3a9510f5f33e17f652de973beef247b543e53c79
‘lrj820ʼ <19876321348@163.com>
2024-07-18 09:22:35 +0800

    登陆-token拦截器中携带

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useUserStore} from '@/stores/use'
 const instance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000
   })
 
   instance.interceptors.request.use(config => {
+    const useStore = useUserStore()
+    const token = useStore.userInfo.token
+    if (token){
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   }, e => Promise.reject(e))
   
