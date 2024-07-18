2e2ba7364ede7d0ca735a9a2ffbc40096a4df0f4
‘lrj820ʼ <19876321348@163.com>
2024-07-18 14:58:32 +0800

    头部购物车-列表购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -18,6 +18,12 @@ export const useCartStore = defineStore('cart',() =>{
     const idx = cartList.value.findIndex((item) => skuId === item.skuId)
     cartList.value.splice(idx, 1)
 }
+// 单选功能
+const singleCheck = (skuId, selected) => {
+    // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+    const item = cartList.value.find((item) => item.skuId === skuId)
+    item.selected = selected
+  }
 const allCount = computed(() => cartList.value.reduce((a,c) => a + c.count,0))
 const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.price,0))
     return{
@@ -25,7 +31,8 @@ const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.p
         allPrice,
         allCount,
         addcart,
-        delCart
+        delCart,
+        singleCheck
     }
 },{
     persist: true,
--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { useCartStore } from '@/stores/cartStore';
 const cartStore = useCartStore()
+const singleCheck = (i, selected) => {
+  console.log(i, selected)
+  // store cartList 数组 无法知道要修改谁的选中状态？
+  // 除了selected补充一个用来筛选的参数 - skuId
+  cartStore.singleCheck(i.skuId, selected)
+}
 </script>
 
 <template>
@@ -24,7 +30,7 @@ const cartStore = useCartStore()
           <tbody>
             <tr v-for="i in cartStore.cartList" :key="i.id">
               <td>
-                <el-checkbox />
+                <el-checkbox :model-value="i.selected" @change="(selected) => singleCheck(i, selected)" />
               </td>
               <td>
                 <div class="goods">
